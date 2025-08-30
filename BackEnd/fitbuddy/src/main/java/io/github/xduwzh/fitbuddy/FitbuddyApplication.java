e0200eef3a8a771be4b7c358c02525e18abd53ed
xduwzh <128497005+xduwzh@users.noreply.github.com>
2025-08-30 13:53:44 +0200

    Proj init

---
+++ BackEnd/fitbuddy/src/main/java/io/github/xduwzh/fitbuddy/FitbuddyApplication.java
@@ -0,0 +1,13 @@
+package io.github.xduwzh.fitbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbuddyApplication.class, args);
+	}
+
+}
---
+++ BackEnd/fitbuddy/src/test/java/io/github/xduwzh/fitbuddy/FitbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xduwzh.fitbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
